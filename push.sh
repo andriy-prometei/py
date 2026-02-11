4824f7209a9cb369a1ad39569c5e4a5de35dd78b
Andriy <andriyprometei@gmail.com>
2026-02-11 17:49:16 +0200

    auto 2026-02-11 15:49:15

--- push.sh
+++ push.sh
@@ -2,6 +2,14 @@
 
 git config --global --add safe.directory /mnt/android/_dl/py
 
+# 1. Налаштувати credential helper (зберігатиме username + token у файлі \~/.git-credentials)
+#git config --global credential.helper store
+
+# 2. Перевірити, що налаштування збереглося
+#git config --global --get credential.helper
+# має вивести: store
+
+
 echo "→ Перехід до /mnt/android/_dl/py"
 cd /mnt/android/_dl/py || { echo "Не можу перейти в /mnt/android/_dl/py"; exit 1; }
 
